b1125f8142829ab8ea2d344aab7b19c884ef7263
dydwls <dydwls159951@gmail.com>
2018-06-29 03:44:33 +0900

    으아아아

---
+++ Pomodoro/GroupListViewContoller.swift
@@ -0,0 +1,27 @@
+//
+//  GroupListViewContoller.swift
+//  Pomodoro
+//
+//  Created by 권용진 on 2018. 6. 29..
+//  Copyright © 2018년 Jin. All rights reserved.
+//
+
+import UIKit
+
+class GroupListViewController: UIViewController {
+    
+    @IBOutlet weak var GroupTable: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
+
