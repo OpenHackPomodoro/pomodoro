a707d995b9a71ac64924aefd47823739c2e79673
bbvch13531 <bbvch13531@gmail.com>
2018-06-30 01:34:15 +0900

    Install popupdialog
    Signed-off-by: bbvch13531 <bbvch13531@gmail.com>

--- iOS/PomodoroTimer/Pods/Target Support Files/Pods-PomodoroTimer/Pods-PomodoroTimer-frameworks.sh
+++ iOS/PomodoroTimer/Pods/Target Support Files/Pods-PomodoroTimer/Pods-PomodoroTimer-frameworks.sh
@@ -144,11 +144,15 @@ strip_invalid_archs() {
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/Alamofire/Alamofire.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/DynamicBlurView/DynamicBlurView.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/KDCircularProgress/KDCircularProgress.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/PopupDialog/PopupDialog.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/Alamofire/Alamofire.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/DynamicBlurView/DynamicBlurView.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/KDCircularProgress/KDCircularProgress.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/PopupDialog/PopupDialog.framework"
 fi
 if [ "${COCOAPODS_PARALLEL_CODE_SIGN}" == "true" ]; then
   wait
