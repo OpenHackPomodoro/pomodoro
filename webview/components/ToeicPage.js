ad8a44e0e28b431181cb867a06be622a24d3e1fa
hera0109 <hela0109@naver.com>
2018-06-30 04:15:12 +0900

    add page

---
+++ webview/components/ToeicPage.js
@@ -0,0 +1,110 @@
+import React from 'react';
+import Header from './Header';
+
+const ToeicPage = () => (
+  <div className='o'>
+    <Header title='토익' />
+    <div className='guest-text'>뽀모도로 참여자</div>
+    <div className='guest-images'>
+      <div className='guest-image'><div /></div>
+      <div className='guest-image'><div /></div>
+      <div className='guest-image'><div /></div>
+      <div className='guest-image'><div /></div>
+      <div className='guest-image'><div /></div>
+      <div className='guest-image'><div /></div>
+    </div>
+    <div className='guest-description'>5명의 참여자</div>
+    <div className='league-description'>오늘 개별 리그</div>
+    <div className='tttt'>
+      <div className='user-item'>
+        <div className='guest-info'>
+          <div />
+          <div className='user-name'>권용진</div>
+          <div className='user-rank'>1위</div>
+          <div className='online' />
+        <div />
+        </div>
+        <div className='guest-stat'>
+          <div className='guest-message'>"중국 가즈아"</div>
+          <div className='bar-container'>
+            <div className='guest-bar'><div><div className='cccc'><div className='bar-inner-1'></div><div className='aaaa'/></div></div></div>
+            <div className='user-count'>7/10</div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div className='tttt'>
+      <div className='user-item'>
+        <div className='guest-info'>
+          <div />
+          <div className='user-name'>신예진</div>
+          <div className='user-rank'>2위</div>
+        <div />
+        </div>
+        <div className='guest-stat'>
+          <div className='guest-message'>"뽀모도로로 토익 980 ㄱㄱ"</div>
+          <div className='bar-container'>
+            <div className='guest-bar'><div className='bar-inner-2'/></div>
+            <div className='user-count'>6/10</div>
+          </div>
+        </div>
+      </div>
+    </div>
+     <div className='tttt'>
+      <div className='user-item'>
+        <div className='guest-info'>
+          <div />
+          <div className='user-name'>허경영</div>
+          <div className='user-rank'>3위</div>
+        <div />
+        </div>
+        <div className='guest-stat'>
+          <div className='guest-message'>"상 받고 싶어요"</div>
+          <div className='bar-container'>
+            <div className='guest-bar'><div className='bar-inner-3'/></div>
+            <div className='user-count'>4/10</div>
+          </div>
+        </div>
+      </div>
+    </div>
+ <div className='tttt'>
+      <div className='user-item'>
+        <div className='guest-info'>
+          <div />
+          <div className='user-name'>허준희</div>
+        <div />
+        </div>
+        <div className='guest-stat'>
+          <div className='guest-message'>"오늘도 화이팅"</div>
+          <div className='bar-container'>
+            <div className='guest-bar'><div className='bar-inner-4'/></div>
+            <div className='user-count'>3/10</div>
+          </div>
+        </div>
+      </div>
+    </div>
+ <div className='tttt'>
+      <div className='user-item'>
+        <div className='guest-info'>
+          <div />
+          <div className='user-name'>홍순우</div>
+          <div className='online' />
+        <div />
+        </div>
+        <div className='guest-stat'>
+          <div className='guest-message'>"뽀모도로 화이팅"</div>
+          <div className='bar-container'>
+            <div className='guest-bar'><div className='bar-inner-5'/></div>
+            <div className='user-count'>1/10</div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+
+
+  </div>
+);
+
+export default ToeicPage;
+
